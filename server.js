40598bda462efde4d632c3d3611b16eb85c3d08a
bcmiller713 <bcmiller713@gmail.com>
2017-07-21 22:24:09 -0700

    prep for heroku

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var bodyParser = require("body-parser");
 var path = require("path");
 
 var app = express();
-var port = process.env.port || 8080;
+var PORT = process.env.PORT || 8080;
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -14,6 +14,6 @@ app.use(bodyParser.json({ type: "application/vnd.api+json" }));
 require("./app/routing/apiRoutes.js")(app);
 require("./app/routing/htmlRoutes.js")(app);
 
-app.listen(port, function () {
-    console.log("App listening on port " + port);
+app.listen(PORT, function () {
+    console.log("App listening on port " + PORT);
 });
